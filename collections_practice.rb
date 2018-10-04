9542c6f75949803ffe446ee4d2e4f6dfafe656f0
Megan Farley <megfarley4@gmail.com>
2018-10-04 18:16:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,8 +49,7 @@ end
 
 #count how many times something appears in an array  
 def count_elements(array)
-  temp = Hash.new(0)
-  array.each_with_object(temp) { |item, i| item[i[:name]] += 1 }.
+  array.each_with_object(Hash.new(0)) { |item, i| item[i[:name]] += 1 }.
     map { |name, count| { :name=>name, :count=>count } }
 end
 
